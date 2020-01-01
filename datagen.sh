1ad419ab662553a50738dc3fa95d148a34dd7abe
mohammad dehghan <mohamad.dehghan12@yahoo.com>
2020-01-02 00:44:05 +0330

    beleive me nothing

--- datagen.sh
+++ datagen.sh
@@ -16,7 +16,7 @@
 python2 run.py \
 		--generate_data=True \
 		--data_dir=./data-sb \
-		--tmp_dir=/data/lzhou/t2t/interactive-decoding/c2e/data-trans \
+		--tmp_dir=./data/lzhou/t2t/interactive-decoding/c2e/data-trans \
 		--train_src_name=2m.bpe.unk.zh \
 		--train_tgt_name=2m.bpe.unk.en \
 		--vocab_src_size=30720 \
