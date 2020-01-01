b3220d2af1dab5487c0211f8cb4519afd0d16822
mohammad dehghan <mohamad.dehghan12@yahoo.com>
2020-01-01 15:52:06 +0330

    routes in datagen.sh are updated

--- datagen.sh
+++ datagen.sh
@@ -2,8 +2,8 @@
 
 python run.py \
 		--generate_data=True \
-		--data_dir=./data-sb \
-		--tmp_dir=/data/lzhou/t2t/interactive-decoding/c2e/data-trans \
+		--data_dir=../data \
+		--tmp_dir=../data/tmp-data \
 		--train_src_name=2m.bpe.unk.zh \
 		--train_tgt_name=2m.bpe.unk.en \
 		--vocab_src_size=30720 \
