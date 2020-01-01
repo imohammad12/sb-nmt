099b088150e88af3c8e6f317bc1873d7db45f8ab
mohammad dehghan <mohamad.dehghan12@yahoo.com>
2020-01-01 18:01:45 +0330

    just testing

--- datagen.sh
+++ datagen.sh
@@ -4,11 +4,12 @@ python3.6 ./run.py \
 		--generate_data=True \
 		--data_dir=../data \
 		--tmp_dir=../data/tmp-data \
-		--train_src_name=2m.bpe.unk.zh \
-		--train_tgt_name=2m.bpe.unk.en \
+		--train_src_name=train.tok.clean.bpe.32000.de \
+		--train_tgt_name=train.tok.clean.bpe.32000.en \
 		--vocab_src_size=30720 \
 		--vocab_tgt_size=30720 \
 		--vocab_src_name=vocab.bpe.zh \
 		--vocab_tgt_name=vocab.bpe.en \
-		--num_shards=50 
+		--num_shards=50
+		# 2m.bpe.unk.en
 
