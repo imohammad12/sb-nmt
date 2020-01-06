c17e4410eab426792ca438cc4cd803faed29f017
mohammad dehghan <mohamad.dehghan12@yahoo.com>
2020-01-06 17:39:16 +0330

    path corrected2

--- datagen.sh
+++ datagen.sh
@@ -2,13 +2,13 @@
 
 python2 run.py \
 		--generate_data=True \
-		--data_dir=/content/gdrive/My\ Drive/sb-nmt/sb-nmt/data \
-		--tmp_dir=/content/gdrive/My\ Drive/sb-nmt/sb-nmt/data/tmp-data \
+		--data_dir=/content/gdrive/My\ Drive/sb-nmt/data \
+		--tmp_dir=/content/gdrive/My\ Drive/sb-nmt/data/tmp-data \
 		--train_src_name=train.de \
 		--train_tgt_name=train.en \
 		--vocab_src_size=50000 \
 		--vocab_tgt_size=50000 \
-		--vocab_src_name=vocab.50K.de \
+		--vocab_src_name=vocab.50K.de vocab.50K.de \
 		--vocab_tgt_name=vocab.50K.en \
 		--num_shards=50 \
 		--hparams_set=transformer_params_base
