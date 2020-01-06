c79e79ab9d3eea8484811d0d2d94c6b4ca6948fb
mohammad dehghan <mohamad.dehghan12@yahoo.com>
2020-01-06 17:29:07 +0330

    new data

--- datagen.sh
+++ datagen.sh
@@ -2,14 +2,14 @@
 
 python2 run.py \
 		--generate_data=True \
-		--data_dir=../data \
-		--tmp_dir=../data/tmp-data \
-		--train_src_name=train.tok.clean.bpe.32000.de \
-		--train_tgt_name=train.tok.clean.bpe.32000.en \
-		--vocab_src_size=32000 \
-		--vocab_tgt_size=32000 \
-		--vocab_src_name=vocab.32K.de \
-		--vocab_tgt_name=vocab.32K.en \
+		--data_dir=/content/gdrive/My Drive/sb-nmt/sb-nmt/data \
+		--tmp_dir=/content/gdrive/My Drive/sb-nmt/sb-nmt/data/tmp-data \
+		--train_src_name=train.de \
+		--train_tgt_name=train.en \
+		--vocab_src_size=50000 \
+		--vocab_tgt_size=50000 \
+		--vocab_src_name=vocab.50K.de \
+		--vocab_tgt_name=vocab.50K.en \
 		--num_shards=50 \
 		--hparams_set=transformer_params_base
 		# 2m.bpe.unk.en
